eac49e70dd55f98bb4025a33705714261d7da5b4
Suui <suui.gd@gmail.com>
2017-05-07 00:37:17 +0200

    Red: As_a_user_I_want_to_create_folders_in_the_Vocabulary_section_of_my_Workspace

---
+++ Test/End2End/CreateFolders.cs
@@ -0,0 +1,49 @@
+﻿using System.Linq;
+using FluentAssertions;
+using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
+
+
+namespace Test.End2End
+{
+	[TestFixture]
+	class CreateFolders
+	{
+		private ChromeDriver Browser;
+
+		[SetUp]
+		public void given_a_browser()
+		{
+			SetupWebDriver();
+			Browser = new ChromeDriver();
+		}
+
+		[Test]
+		public void As_a_user_I_want_to_create_folders_in_the_Vocabulary_section_of_my_Workspace()
+		{
+			const string vocabularyTableName = "Parrot Words";
+
+			Browser.Navigate().GoToUrl("http://localhost:57143");
+			Browser.Url.Should().Be("http://localhost:57143/login");
+
+			Browser.FindElementByName("username").SendKeys("CrazyParrot");
+			Browser.FindElementByName("password").SendKeys("parroty_pass");
+			Browser.FindElementByName("submitLogin").Click();
+			Browser.Url.Should().Be("http://localhost:57143");
+
+			Browser.FindElementByCssSelector("#workspace #vocabulary .add").Click();
+			Browser.FindElementByName("vocabulary-table-title").SendKeys(vocabularyTableName);
+			Browser.FindElementByName("create-vocabulary-table").Click();
+
+			var folders = Browser.FindElementsByCssSelector("#workspace #vocabulary .folder");
+			folders.First().Text.Should().Be(vocabularyTableName);
+		}
+
+		private static void SetupWebDriver()
+		{
+			new DriverManager().SetUpDriver(new ChromeConfig());
+		}
+	}
+}
