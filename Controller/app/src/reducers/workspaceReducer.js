ad0fcc078bce32c77f385e410e28eac7122d0afa
Suui <suui.gd@gmail.com>
2017-05-28 03:09:53 +0200

    Refactor reducer to map

--- Controller/app/src/reducers/workspaceReducer.js
+++ Controller/app/src/reducers/workspaceReducer.js
@@ -1,35 +1,35 @@
 import * as types from '../actions/types/workspace';
 
 export default function workspaceReducer(workspace = { vocabularyFolders: [] }, action) {
-    if (action.type == types.VOCABULARY_FOLDER_CREATED) {
-        return vocabularyFolderCreated(workspace, action);
-    }
-    if (action.type == types.VOCABULARY_FOLDERS_RETRIEVED) {
-        return vocabularyFoldersRetrieved(workspace, action);
-    }
-    switch (action.type) {
-        case types.CREATE_VOCABULARY_FOLDER:
-            if (workspace.vocabularyFolders) {
-                const vocabularyFolders = [...workspace.vocabularyFolders, action.folderName];
-                return Object.assign({}, workspace, { vocabularyFolders });
-            }
-            return Object.assign({}, workspace, { vocabularyFolders: [action.folderName] });
-        
-        default:
-            return workspace;
-    }
-}
 
-function vocabularyFolderCreated(workspace, action) {
-    const folder = Object.assign({}, action.folder);
-    if (workspace.vocabularyFolders) {
-        const vocabularyFolders = [...workspace.vocabularyFolders, folder];
-        return Object.assign({}, workspace, { vocabularyFolders });
+    function vocabularyFolderCreated() {
+        const folder = Object.assign({}, action.folder);
+        if (workspace.vocabularyFolders) {
+            const vocabularyFolders = [...workspace.vocabularyFolders, folder];
+            return Object.assign({}, workspace, { vocabularyFolders });
+        }
+        return Object.assign({}, workspace, { vocabularyFolders: [folder] });
     }
-    return Object.assign({}, workspace, { vocabularyFolders: [folder] });
-}
 
-function vocabularyFoldersRetrieved(workspace, action) {
-    const folders = [...action.folders];
-    return Object.assign({}, workspace, { vocabularyFolders: folders });
+    function vocabularyFoldersRetrieved() {
+        const folders = [...action.folders];
+        return Object.assign({}, workspace, { vocabularyFolders: folders });
+    }
+
+    function createVocabularyFolder() {
+        const folder = Object.assign({}, action.folder);
+        if (workspace.vocabularyFolders) {
+            const vocabularyFolders = [...workspace.vocabularyFolders, folder];
+            return Object.assign({}, workspace, { vocabularyFolders });
+        }
+        return Object.assign({}, workspace, { vocabularyFolders: [folder] });
+    }
+
+    const execute = {
+        [types.VOCABULARY_FOLDER_CREATED]: () => vocabularyFolderCreated(),
+        [types.VOCABULARY_FOLDERS_RETRIEVED]: () => vocabularyFoldersRetrieved(),
+        [types.CREATE_VOCABULARY_FOLDER]: () => createVocabularyFolder()
+    };
+    
+    return execute[action.type] ? execute[action.type]() : workspace;
 }
